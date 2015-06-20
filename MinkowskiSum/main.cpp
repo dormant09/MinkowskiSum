2cfc27a6c002d32731bbaafce972facf774d4bae
Kim Sijoon <dormant09@snu.ac.kr>
2015-06-21 00:05:46 +0900

    Initial Commit

---
+++ MinkowskiSum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MinkowskiSum
+//
+//  Created by Sijoon's MacBookAir on 2015. 6. 21..
+//  Copyright (c) 2015년 Sijoon's MacBookAir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
